d36c651fb490f1922b7a5481affdb48d74573710
Ivan Liu <ivan.liuyanfeng@gmail.com>
2017-06-22 22:44:11 +1000

    :more features:

--- python/feature_engineering.py
+++ python/feature_engineering.py
@@ -6,10 +6,15 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.decomposition import PCA, FastICA, TruncatedSVD
 from sklearn.random_projection import SparseRandomProjection, GaussianRandomProjection
 from sklearn.metrics import r2_score
+from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def feat_standardize(x):
+    x = x.apply(lambda x: x/(x.max()-x.min()))
+    return x
+    
 # Read data in
 train = pd.read_csv("./data/train.csv")
 test = pd.read_csv("./data/test.csv")
@@ -34,12 +39,24 @@ test.head()
 all_dt = pd.concat([train, test])
 # Standized
 cat_col = [col for col in all_dt.columns if col not in ['y'] and all_dt[col].max()>1]
-all_dt[cat_col] = all_dt[cat_col].apply(lambda x: x/(x.max()-x.min()))
+all_dt[cat_col] = feat_standardize(all_dt[cat_col])
 all_dt.tail()
+
+# 6. Interaction features
+interact_feat = pd.DataFrame()
+for i in all_dt.columns:
+    for j in all_dt.columns:
+        yi = np.abs(np.corrcoef(train[i], train['y'])[0,1])
+        yj = np.abs(np.corrcoef(train[j], train['y'])[0,1])
+        yij = np.abs(np.corrcoef(train[j]+train[i], train['y'])[0,1])
+        if yij > yi + yj:
+            print(i+'_'+j+': '+str(yi)+' '+str(yj)+' '+str(yij))
+            interact_feat['Itr_'+i+'_'+j] = all_dt[j]+all_dt[i]
+interact_feat = feat_standardize(interact_feat)            
 
 # 2. PCA, ICA & SVD
 n_comp = 16
-random_seed = 430
+random_seed = 624
 # 2.1 PCA
 pca = PCA(n_components=n_comp, random_state=random_seed)
 pca_feat = pca.fit_transform(all_dt.drop(['y'], axis = 1))
@@ -52,6 +69,10 @@ svd_feat = svd.fit_transform(all_dt.drop(['y'], axis = 1))
 # 2.4 TSNE
 tsne = TSNE(n_components=3, random_state=random_seed, verbose = 1)
 tsne_feat = tsne.fit_transform(all_dt.drop(['y'], axis = 1))
+# 2.5 KMeans
+kmeans = KMeans(n_clusters=4, random_state=random_seed)
+kmeans_feat = kmeans.fit_transform(all_dt.drop(['y'], axis = 1))
+# 2.6 Logistic PCA
 
 # 3. Random Projection
 # 3.1 SRP
@@ -65,21 +86,31 @@ grp_feat = grp.fit_transform(all_dt.drop(['y'], axis = 1))
 # 4.1 PCA
 pca_feat_dat = pd.DataFrame(pca_feat)
 pca_feat_dat = pca_feat_dat.rename(columns=lambda x: 'pca_'+str(x))
+pca_feat_dat  = feat_standardize(pca_feat_dat)
 # 4.2 ICA
 ica_feat_dat = pd.DataFrame(ica_feat)
 ica_feat_dat = ica_feat_dat.rename(columns=lambda x: 'ica_'+str(x))
+ica_feat_dat  = feat_standardize(ica_feat_dat)
 # 4.3 SVD
 svd_feat_dat = pd.DataFrame(svd_feat)
 svd_feat_dat = svd_feat_dat.rename(columns=lambda x: 'svd_'+str(x))
+svd_feat_dat  = feat_standardize(svd_feat_dat)
 # 4.4 SRP
 srp_feat_dat = pd.DataFrame(srp_feat)
 srp_feat_dat = srp_feat_dat.rename(columns=lambda x: 'srp_'+str(x))
+srp_feat_dat  = feat_standardize(srp_feat_dat)
 # 4.5 GRP
 grp_feat_dat = pd.DataFrame(grp_feat)
 grp_feat_dat = grp_feat_dat.rename(columns=lambda x: 'grp_'+str(x))
+grp_feat_dat  = feat_standardize(grp_feat_dat)
 # 4.6 TSNE
 tsne_feat_dat = pd.DataFrame(tsne_feat)
 tsne_feat_dat = tsne_feat_dat.rename(columns=lambda x: 'tsne_'+str(x))
+tsne_feat_dat  = feat_standardize(tsne_feat_dat)
+# 4.7 KMeans
+kmeans_feat_dat = pd.DataFrame(kmeans_feat)
+kmeans_feat_dat = kmeans_feat_dat.rename(columns=lambda x: 'kmeans_'+str(x))
+kmeans_feat_dat = feat_standardize(kmeans_feat_dat)
 
 # Merge
 all_dt = pd.concat([all_dt.reset_index(drop=True), pca_feat_dat], axis=1)
@@ -88,27 +119,26 @@ all_dt = pd.concat([all_dt.reset_index(drop=True), svd_feat_dat], axis=1)
 all_dt = pd.concat([all_dt.reset_index(drop=True), srp_feat_dat], axis=1)
 all_dt = pd.concat([all_dt.reset_index(drop=True), grp_feat_dat], axis=1)
 all_dt = pd.concat([all_dt.reset_index(drop=True), tsne_feat_dat], axis=1)
+all_dt = pd.concat([all_dt.reset_index(drop=True), kmeans_feat_dat], axis=1)
+all_dt = pd.concat([all_dt.reset_index(drop=True), interact_feat.reset_index(drop=True)], axis=1)
 all_dt.head()
 
 # 5. Remove useless columns
 toRM_Col = [c for c in train.columns if len(set(train[c]))==1]
 all_dt = all_dt.drop(toRM_Col, axis = 1)
 
-# 6. Interaction features
-
 # 7. Target Mean
 
-# 8. K means
+# 8. Outliers
 
-# 9. Outliers
-
-# 10. Modeling
+# 9. Modeling
 train = all_dt[all_dt['y'] > 0]
 test = all_dt[all_dt['y'] == -1]
 y_train = train['y'].values
 y_mean = np.mean(y_train)
 features = [c for c in train.columns if c != 'y']
-
+#features = list(set(features))
+# features = features_score.index
 # 10.1 Xgboost
 def the_metric(y_pred, y):
     y_true = y.get_label()
@@ -116,14 +146,14 @@ def the_metric(y_pred, y):
 
 xgb_params = {
     'max_depth': 2 # 4
-    ,'eta': 0.005 #0.0045,
+    ,'eta': 0.002 #0.0045,
     ,'objective': 'reg:linear'
     ,'eval_metric': 'rmse'
     ,'booster': 'gbtree'
-    ,'gamma': 1
+    ,'gamma': 10
     ,'min_child_weight': 0
-    ,'subsample': 0.93
-    ,'colsample_bytree': 1
+    ,'subsample': 1
+    ,'colsample_bytree': 0.7
     ,'lambda': 2
     ,'alpha': 1
     ,'base_score': y_mean
@@ -148,7 +178,7 @@ model = xgb.train(dict(xgb_params),
                verbose_eval=100, 
                dtrain = dtrain, 
                num_boost_round=model.shape[0])
-features_score = pd.Series(model.get_fscore()).sort_values(ascending=False)[:50]
+features_score = pd.Series(model.get_fscore()).sort_values(ascending=False)#[:80]
 plt.figure(figsize=(7,10))
 sns.barplot(x=features_score.values, y=features_score.index.values, orient='h')
 
@@ -156,4 +186,4 @@ sns.barplot(x=features_score.values, y=features_score.index.values, orient='h')
 y_pred = model.predict(dtest)
 
 output = pd.DataFrame({'id': test_id.astype(np.int32), 'y': y_pred})
-output.to_csv('./prediction/python_xgb_1.csv', index=False)
+output.to_csv('./prediction/python_xgb_3.csv', index=False)
